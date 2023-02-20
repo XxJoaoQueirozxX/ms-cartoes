bb27101753bf96815b94784613f22568182df29e
Joao Queiroz <joaovic.pqueiroz@gmail.com>
2023-02-20 20:19:31 -0300

    refactor ms cartoes

--- ms-cartoes/src/main/java/com/ms/cartoes/mscartoes/resources/CardController.java
+++ ms-cartoes/src/main/java/com/ms/cartoes/mscartoes/resources/CardController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,7 +32,7 @@ public class CardController {
     }
 
     @PostMapping
-    public ResponseEntity register(@RequestBody CardRegisterRequest request) {
+    public ResponseEntity register(@RequestBody @Valid CardRegisterRequest request) {
         final Card card = request.toCard();
         cardService.save(card);
         return ResponseEntity.status(HttpStatus.CREATED).build();
--- ms-cartoes/src/main/java/com/ms/cartoes/mscartoes/resources/dtos/CardRegisterRequest.java
+++ ms-cartoes/src/main/java/com/ms/cartoes/mscartoes/resources/dtos/CardRegisterRequest.java
@@ -4,14 +4,23 @@ import com.ms.cartoes.mscartoes.entities.Card;
 import com.ms.cartoes.mscartoes.entities.enums.CardBrand;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
 public class CardRegisterRequest {
 
+    @NotBlank(message = "{card.register.request.name.notblank}")
     private String name;
+
+    @NotNull(message = "{card.register.request.brand.notnull}")
     private CardBrand brand;
+
+    @NotNull(message = "{card.register.request.income.notnull}")
     private BigDecimal income;
+
+    @NotNull(message = "{card.register.request.initialLimit.notnull}")
     private BigDecimal initialLimit;
 
 
